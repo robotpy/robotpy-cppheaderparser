77a01cdce1e13896795c39ea197b9b086a225fff
senexcanis <>
2010-11-14 02:50:29 +0000

    Updated example with doxygen comments to demonstrate how they work.
    Gave examples of accessing various types of data from different methods and parameters.

--- CppHeaderParser/examples/SampleClass.h
+++ CppHeaderParser/examples/SampleClass.h
@@ -5,12 +5,37 @@ class SampleClass
 {
 public:
     SampleClass();
+    /*!
+     * Method 1
+     */
     string meth1();
+
+    ///
+    /// Method 2 description
+    ///
+    /// @param v1 Variable 1
+    ///
     int meth2(int v1);
+
+    /**
+     * Method 3 description
+     *
+     * \param v1 Variable 1
+     * \param v2 Variable 2
+     */
     void meth3(const string & v1, vector<string> & v2);
+
+    /**********************************
+     * Method 4 description
+     *
+     * @return Return value
+     *********************************/
     unsigned int meth4();
 private:
     void * meth5(){return NULL};
+
+    /// prop1 description
     string prop1;
+    //! prop5 description
     int prop5;
 };
--- CppHeaderParser/examples/readSampleClass.py
+++ CppHeaderParser/examples/readSampleClass.py
@@ -13,3 +13,24 @@ print "Number of private properties %d"%(len(sampleClass["properties"]["private"
 meth3 = [m for m in sampleClass["methods"]["public"] if m["name"] == "meth3"][0] #get meth3
 meth3ParamTypes = [t["type"] for t in meth3["parameters"]] #get meth3s parameters
 print "Parameter Types for public method meth3 %s"%(meth3ParamTypes)
+
+print "\nReturn type for meth1:"
+print cppHeader.classes["SampleClass"]["methods"]["public"][1]["rtnType"]
+
+print "\nDoxygen for meth2:"
+print cppHeader.classes["SampleClass"]["methods"]["public"][2]["doxygen"]
+
+print "\nParameters for meth3:"
+print cppHeader.classes["SampleClass"]["methods"]["public"][3]["parameters"]
+
+print "\nDoxygen for meth4:"
+print cppHeader.classes["SampleClass"]["methods"]["public"][4]["doxygen"]
+
+print "\nReturn type for meth5:"
+print cppHeader.classes["SampleClass"]["methods"]["private"][0]["rtnType"]
+
+print "\nDoxygen type for prop1:"
+print cppHeader.classes["SampleClass"]["properties"]["private"][0]["doxygen"]
+
+print "\nType for prop5:"
+print cppHeader.classes["SampleClass"]["properties"]["private"][1]["type"]
