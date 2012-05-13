4f2aac007988d2a8c940aa2047474c30f4680ed0
senex <>
2012-05-13 14:26:11 -0400

    Change version number to 2.1

--- CppHeaderParser/CppHeaderParser.py
+++ CppHeaderParser/CppHeaderParser.py
@@ -59,7 +59,7 @@ def lineno():
     """Returns the current line number in our program."""
     return inspect.currentframe().f_back.f_lineno
 
-version = __version__ = "2.0"
+version = __version__ = "2.1"
 
 tokens = [
     'NUMBER',
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ CLASSIFIERS = [
 
 setup(
     name = 'CppHeaderParser',
-    version = '2.0',
+    version = '2.1',
     author = 'Jashua Cloutier',
     author_email = 'jashuac@bellsouth.net',
     url = 'http://senexcanis.com/open-source/cppheaderparser/',
