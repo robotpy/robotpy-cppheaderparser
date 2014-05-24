dbf92b3851697e62dcbddc7f20c2a5b7f619a742
senex <>
2014-05-24 19:20:29 -0400

    Change version number to 2.4.2

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ CLASSIFIERS = [
 
 setup(
     name = 'CppHeaderParser',
-    version = '2.4.1',
+    version = '2.4.2',
     author = 'Jashua Cloutier',
     author_email = 'jashuac@bellsouth.net',
     url = 'http://senexcanis.com/open-source/cppheaderparser/',
