b88f412f9b34a47c8222dae7e5e7384b8794cd85
senex <>
2015-04-18 12:09:40 -0400

    Set the version number to 2.5.1

--- CppHeaderParser/CppHeaderParser.py
+++ CppHeaderParser/CppHeaderParser.py
@@ -59,7 +59,7 @@ def lineno():
     """Returns the current line number in our program."""
     return inspect.currentframe().f_back.f_lineno
 
-version = __version__ = "2.5d"
+version = __version__ = "2.5.1"
 
 tokens = [
     'NUMBER',
