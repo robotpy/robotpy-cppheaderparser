bea0b5aa91456229806d7a2c1d536df865bf1afd
Dustin Spicuzza <dustin@virtualroadside.com>
2019-09-24 23:18:48 -0400

    Format code with black
    - Fixes #8

--- setup.py
+++ setup.py
@@ -39,42 +39,43 @@ with open(version_file, "r") as fp:
     exec(fp.read(), globals())
 
 DESCRIPTION = (
-    'Parse C++ header files and generate a data structure '
-    'representing the class'
-    )
+    "Parse C++ header files and generate a data structure " "representing the class"
+)
 
 
 CLASSIFIERS = [
-    'Operating System :: OS Independent',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: C++',
-    'License :: OSI Approved :: BSD License',
-    'Development Status :: 5 - Production/Stable',
-    'Intended Audience :: Developers',
-    'Topic :: Software Development',
-    'Topic :: Software Development :: Code Generators',
-    'Topic :: Software Development :: Compilers',
-    'Topic :: Software Development :: Disassemblers'
-    ]
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: C++",
+    "License :: OSI Approved :: BSD License",
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "Topic :: Software Development",
+    "Topic :: Software Development :: Code Generators",
+    "Topic :: Software Development :: Compilers",
+    "Topic :: Software Development :: Disassemblers",
+]
 
 setup(
-    name = 'robotpy-cppheaderparser',
-    version = __version__,
-    author = 'Jashua Cloutier',
-    author_email = 'jashuac@bellsouth.net',
-    maintainer = 'RobotPy Development Team',
-    maintainer_email = 'robotpy@googlegroups.com',
-    url = 'https://github.com/robotpy/robotpy-cppheaderparser',
-    description = DESCRIPTION,
-    long_description = open('README.md').read(),
-    license = 'BSD',
-    platforms = 'Platform Independent',
-    packages = ['CppHeaderParser'],
-    keywords = 'c++ header parser ply',
-    classifiers = CLASSIFIERS,
-    requires = ['ply'],
-    install_requires=['ply'],
-    package_data = { 'CppHeaderParser': ['README', 'README.html', 'doc/*.*', 'examples/*.*'], },
-    )
+    name="robotpy-cppheaderparser",
+    version=__version__,
+    author="Jashua Cloutier",
+    author_email="jashuac@bellsouth.net",
+    maintainer="RobotPy Development Team",
+    maintainer_email="robotpy@googlegroups.com",
+    url="https://github.com/robotpy/robotpy-cppheaderparser",
+    description=DESCRIPTION,
+    long_description=open("README.md").read(),
+    license="BSD",
+    platforms="Platform Independent",
+    packages=["CppHeaderParser"],
+    keywords="c++ header parser ply",
+    classifiers=CLASSIFIERS,
+    requires=["ply"],
+    install_requires=["ply"],
+    package_data={
+        "CppHeaderParser": ["README", "README.html", "doc/*.*", "examples/*.*"]
+    },
+)
